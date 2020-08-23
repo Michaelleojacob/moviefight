7339dd69933116e78ad039a53ffc32fadf2a1287
michaeljacob <michaelleojacob@gmail.com>
2020-08-23 11:31:15 -0700

    API works, gets results from omdbapi. uncomment s: whatever to

--- app.js
+++ app.js
@@ -1,1 +1,16 @@
+// let ombd = "http://www.omdbapi.com/?apikey=15c4edfb&";
+// let apikey = "15c4edfb";
 
+const fetchData = async () => {
+  const response = await axios.get("http://www.omdbapi.com", {
+    params: {
+      apikey: "15c4edfb",
+      //s for search
+      // s: "avengers",
+      i: "tt0848228",
+    },
+  });
+  console.log(response.data);
+};
+
+fetchData();
