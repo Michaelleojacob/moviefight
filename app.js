16ccebf657cf76cd422ec20b90821aa728bd322b
michaeljacob <michaelleojacob@gmail.com>
2020-08-23 11:52:45 -0700

    changed the code to allow the user to make the params of our omdb search. No results are shown on the page yet, but they can be seen under the console network tab. XHR tab inside network will display the results of whatever we type in the input field. currently it makes a request per every key stroke. The next objective is to fix that issue, so that only one request is made per entry instead of requests*keystrokes.

--- app.js
+++ app.js
@@ -1,16 +1,22 @@
 // let ombd = "http://www.omdbapi.com/?apikey=15c4edfb&";
 // let apikey = "15c4edfb";
 
-const fetchData = async () => {
+const fetchData = async (searchTerm) => {
   const response = await axios.get("http://www.omdbapi.com", {
     params: {
       apikey: "15c4edfb",
+      s: searchTerm,
       //s for search
       // s: "avengers",
-      i: "tt0848228",
+      // i: "tt0848228",
     },
   });
   console.log(response.data);
 };
 
-fetchData();
+const input = document.querySelector("input");
+
+input.addEventListener("input", (event) => {
+  //access the changed input text. Whatever the user just put into the input.
+  fetchData(event.target.value);
+});
