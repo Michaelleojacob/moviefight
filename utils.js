4b39048b5b1de8e847b50669d0032520610f4bdc
michaeljacob <michaelleojacob@gmail.com>
2020-08-24 16:09:15 -0700

    expanded on whatever movie the user clicks on from the initial search. It now displays a ton of information including awards, box office, metascore, rating and votes

--- utils.js
+++ utils.js
@@ -89,19 +89,45 @@ const onMovieSelect = async (movie) => {
 //movieDetail is the detailed movie info, lots of properties about that movie
 const movieTemplate = (movieDetail) => {
   return `
-<article class= "media">
-    <figure class="media-left">
-        <p class="image">
-            <img src="${movieDetail.Poster}" />
-        </p>
-    </figure>
-    <div class="media-content">
-        <div class="content">
-            <h1>${movieDetail.Title}</h1>
-            <h4>${movieDetail.Genre}</h4>
-            <p>${movieDetail.Plot}</p>
-        </div>
-    </div>
-</article>
-`;
+        <article class= "media">
+            <figure class="media-left">
+                <p class="image">
+                    <img src="${movieDetail.Poster}" />
+                </p>
+            </figure>
+            <div class="media-content">
+                <div class="content">
+                    <h1>${movieDetail.Title}</h1>
+                    <h4>${movieDetail.Genre}</h4>
+                    <p>${movieDetail.Plot}</p>
+                </div>
+            </div>
+        </article>
+
+        <article class="notification is-primary">
+        <p class="title">${movieDetail.Awards}</p>
+        <p class="subtitle">Awards</p>
+        </article>
+
+        <article class="notification is-primary">
+        <p class="title">${movieDetail.BoxOffice}</p>
+        <p class="subtitle">Box Office</p>
+        </article>
+
+        <article class="notification is-primary">
+        <p class="title">${movieDetail.Metascore}</p>
+        <p class="subtitle">Metascore</p>
+        </article>
+
+        <article class="notification is-primary">
+        <p class="title">${movieDetail.imdbRating}</p>
+        <p class="subtitle">IMDB Rating</p>
+        </article>
+
+        <article class="notification is-primary">
+        <p class="title">${movieDetail.imdbVotes}</p>
+        <p class="subtitle">IMDB Votes</p>
+        </article>
+
+        `;
 };
