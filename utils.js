b33643816bfd83f876f7d27b0aff8c99b86dfd4f
michaeljacob <michaelleojacob@gmail.com>
2020-08-24 16:02:20 -0700

    functionality of the movie the user clicks on now displays the poster, title, genre and plot. Everything looks good so far for picking the first movie we want to display information about.

--- utils.js
+++ utils.js
@@ -75,9 +75,7 @@ document.addEventListener("click", (event) => {
   }
 });
 
-//do another request
-//get data
-//render data
+//this 'movie' is just a movie summary, just a handful of properties
 const onMovieSelect = async (movie) => {
   const response = await axios.get("http://www.omdbapi.com/", {
     params: {
@@ -85,5 +83,25 @@ const onMovieSelect = async (movie) => {
       i: movie.imdbID,
     },
   });
-  console.log(response.data);
+  document.querySelector("#summary").innerHTML = movieTemplate(response.data);
+};
+
+//movieDetail is the detailed movie info, lots of properties about that movie
+const movieTemplate = (movieDetail) => {
+  return `
+<article class= "media">
+    <figure class="media-left">
+        <p class="image">
+            <img src="${movieDetail.Poster}" />
+        </p>
+    </figure>
+    <div class="media-content">
+        <div class="content">
+            <h1>${movieDetail.Title}</h1>
+            <h4>${movieDetail.Genre}</h4>
+            <p>${movieDetail.Plot}</p>
+        </div>
+    </div>
+</article>
+`;
 };
