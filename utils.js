2a912fca305c0662529e568d0e6549f4330422e7
michaeljacob <michaelleojacob@gmail.com>
2020-08-25 10:56:20 -0700

    after a user has input two movies, the app will now correctly display which one has better stats and display the winner per each category.

--- utils.js
+++ utils.js
@@ -21,10 +21,33 @@ const onMovieSelect = async (movie, summaryElement, side) => {
 };
 
 const runComparison = () => {
-  console.log("time for a comparison");
   //find the first "article" element for each movie
   //run a comparison on the # of awards
   //then apply some styling to that "article" element
+
+  const leftSideStats = document.querySelectorAll(
+    "#left-summary .notification"
+  );
+  const rightSideStats = document.querySelectorAll(
+    "#right-summary .notification"
+  );
+  //leftstat is going to be each individual stat. and giving them an index.
+  leftSideStats.forEach((leftStat, index) => {
+    const rightStat = rightSideStats[index];
+
+    console.log(leftStat, rightStat);
+
+    const leftSideValue = leftStat.dataset.value;
+    const rightSideValue = rightStat.dataset.value;
+
+    if (rightSideValue > leftSideValue) {
+      leftStat.classList.remove("is-primary");
+      leftStat.classList.add("is-warning");
+    } else {
+      rightStat.classList.remove("is-primary");
+      rightStat.classList.add("is-warning");
+    }
+  });
 };
 
 //movieDetail is the detailed movie info, lots of properties about that movie
@@ -35,7 +58,15 @@ const movieTemplate = (movieDetail) => {
   const metascore = parseInt(movieDetail.Metascore);
   const imdbRating = parseFloat(movieDetail.imdbRating);
   const imdbVotes = parseInt(movieDetail.imdbVotes.replace(/,/g, ""));
-  console.log(dollars, metascore, imdbRating, imdbVotes);
+  let count = 0;
+  const awards = movieDetail.Awards.split(" ").reduce((previous, word) => {
+    const value = parseInt(word);
+    if (isNaN(value)) {
+      return previous;
+    } else {
+      return previous + value;
+    }
+  }, 0);
   return `
         <article class= "media">
             <figure class="media-left">
@@ -52,27 +83,27 @@ const movieTemplate = (movieDetail) => {
             </div>
         </article>
 
-        <article class="notification is-primary">
+        <article data-value=${awards} class="notification is-primary">
         <p class="title">${movieDetail.Awards}</p>
         <p class="subtitle">Awards</p>
         </article>
 
-        <article class="notification is-primary">
+        <article data-value=${dollars} class="notification is-primary">
         <p class="title">${movieDetail.BoxOffice}</p>
         <p class="subtitle">Box Office</p>
         </article>
 
-        <article class="notification is-primary">
+        <article data-value=${metascore} class="notification is-primary">
         <p class="title">${movieDetail.Metascore}</p>
         <p class="subtitle">Metascore</p>
         </article>
 
-        <article class="notification is-primary">
+        <article data-value=${imdbRating} class="notification is-primary">
         <p class="title">${movieDetail.imdbRating}</p>
         <p class="subtitle">IMDB Rating</p>
         </article>
 
-        <article class="notification is-primary">
+        <article data-value=${imdbVotes} class="notification is-primary">
         <p class="title">${movieDetail.imdbVotes}</p>
         <p class="subtitle">IMDB Votes</p>
         </article>
